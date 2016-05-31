d182698ad7546e8d25095cab455ec9cd508bca7d
Rafael Rivera <rafael@withinwindows.com>
2016-05-30 20:21:47 -0700

    Add company metadata to light up taskmgr (closes #63)

--- EarTrumpet/Properties/AssemblyInfo.cs
+++ EarTrumpet/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Ear Trumpet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("File-New-Project")]
 [assembly: AssemblyProduct("Ear Trumpet")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
