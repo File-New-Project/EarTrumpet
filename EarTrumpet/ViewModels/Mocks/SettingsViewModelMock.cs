70e7fbd4546dc2f0bbf4702cc30bf44c2b97338a
Amenta <dave@daveamenta.com>
2017-12-06 17:48:26 -0800

    style tweaks, add mocks

---
+++ EarTrumpet/ViewModels/Mocks/SettingsViewModelMock.cs
@@ -0,0 +1,43 @@
+﻿using EarTrumpet.DataModel;
+using EarTrumpet.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace EarTrumpet.ViewModels.Mocks
+{
+    public class SettingsViewModelMock
+    {
+        class MockAudioSession : BindableBase, IStreamWithVolumeControl
+        {
+            public string DisplayName { get; set; }
+
+            public string Id { get; set; }
+
+            public bool IsMuted { get; set; }
+            public float Volume { get; set; }
+
+            public float PeakValue { get; set; }
+        }
+
+
+        public SettingsViewModelMock()
+        {
+            DefaultApps = new ObservableCollection<AudioSessionViewModel>();
+
+            DefaultApps.Add(new AudioSessionViewModel(new MockAudioSession { DisplayName = "Test 1" }));
+            DefaultApps.Add(new AudioSessionViewModel(new MockAudioSession { DisplayName = "Test 2" }));
+
+            Hotkey = new SettingsService.HotkeyData { Key = System.Windows.Forms.Keys.A, Modifiers = KBModifierKeys.Control | KBModifierKeys.Shift };
+        }
+
+        public ObservableCollection<AudioSessionViewModel> DefaultApps { get; private set; }
+
+        public SettingsService.HotkeyData Hotkey { get; private set; }
+        public string HotkeyText => Hotkey.ToString();
+    }
+}
