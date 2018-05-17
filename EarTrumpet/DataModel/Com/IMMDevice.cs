37b736ae07c99de0217f20e9cfc5093033569c1e
Amenta <dave@daveamenta.com>
2018-05-16 19:14:47 -0700

    Remove/cleanup DataModel.Extensions

--- EarTrumpet/DataModel/Com/IMMDevice.cs
+++ EarTrumpet/DataModel/Com/IMMDevice.cs
@@ -12,4 +12,21 @@ namespace EarTrumpet.DataModel.Com
         void GetId([MarshalAs(UnmanagedType.LPWStr)] out string ppstrId);
         void GetState(out uint pdwState);
     }
+
+    static class IMMDeviceExtensions
+    {
+        public static T Activate<T>(this IMMDevice device)
+        {
+            Guid iid = typeof(T).GUID;
+            device.Activate(ref iid, (uint)CLSCTX.CLSCTX_INPROC_SERVER, IntPtr.Zero, out object ret);
+            return (T)ret;
+        }
+
+        public static string GetId(this IMMDevice device)
+        {
+            string id;
+            device.GetId(out id);
+            return id;
+        }
+    }
 }
