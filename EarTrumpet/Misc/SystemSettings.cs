8edbe789fc2d0a58af6243c30ccda156eaaaf686
Amenta <dave@daveamenta.com>
2018-06-02 17:23:29 -0700

    LightTheme is default when no registry value found

--- EarTrumpet/Misc/SystemSettings.cs
+++ EarTrumpet/Misc/SystemSettings.cs
@@ -7,16 +7,16 @@ namespace EarTrumpet.Misc
     {
         internal static bool IsTransparencyEnabled => ReadPersonalizationSetting("EnableTransparency");
         internal static bool UseAccentColor => ReadPersonalizationSetting("ColorPrevalence");
-        internal static bool IsLightTheme => ReadPersonalizationSetting("AppsUseLightTheme");
+        internal static bool IsLightTheme => ReadPersonalizationSetting("AppsUseLightTheme", 1 /* Light theme is system default */);
 
         internal static bool IsRTL => CultureInfo.CurrentCulture.TextInfo.IsRightToLeft;
 
-        private static bool ReadPersonalizationSetting(string key)
+        private static bool ReadPersonalizationSetting(string key, int defaultValue = 0)
         {
             using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
             using (var subKey = baseKey.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"))
             {
-                return ((int)subKey.GetValue(key, 0)) > 0;
+                return ((int)subKey.GetValue(key, defaultValue)) > 0;
             }
         }
     }
