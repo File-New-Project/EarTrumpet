276490ce8099482d7c1296344240f6719b8e666c
Rafael Rivera <rafael@withinwindows.com>
2018-06-02 20:00:04 -0700

    Remove SHAppBar dependency

--- EarTrumpet/Interop/Shell32.cs
+++ EarTrumpet/Interop/Shell32.cs
@@ -7,37 +7,6 @@ namespace EarTrumpet.Interop
     {
         public const int KF_FLAG_DONT_VERIFY = 0x00004000;
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct APPBARDATA
-        {
-            public int cbSize;
-            public IntPtr hWnd;
-            public uint uCallbackMessage;
-            public uint uEdge;
-            public RECT rc;
-            public int lParam;
-        }
-
-        public enum ABMsg : uint
-        {
-            ABM_NEW = 0,
-            ABM_REMOVE,
-            ABM_QUERYPOS,
-            ABM_SETPOS,
-            ABM_GETSTATE,
-            ABM_GETTASKBARPOS,
-            ABM_ACTIVATE,
-            ABM_GETAUTOHIDEBAR,
-            ABM_SETAUTOHIDEBAR,
-            ABM_WINDOWPOSCHANGED,
-            ABM_SETSTATE
-        }
-
-        [DllImport("shell32.dll", PreserveSig = true)]
-        public static extern IntPtr SHAppBarMessage(
-            ABMsg dwMessage,
-            ref APPBARDATA pData);
-
         [DllImport("shell32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
         [return: MarshalAs(UnmanagedType.Interface)]
         public static extern IShellItem2 SHCreateItemInKnownFolder(
--- EarTrumpet/Misc/WindowsTaskbar.cs
+++ EarTrumpet/Misc/WindowsTaskbar.cs
@@ -1,7 +1,6 @@
 ﻿using EarTrumpet.Interop;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace EarTrumpet.Misc
@@ -23,59 +22,39 @@ namespace EarTrumpet.Misc
             Bottom
         }
 
-        private const string _className = "Shell_TrayWnd";
-
         public static State Current
         {
             get
             {
-                var appbar = new Shell32.APPBARDATA();
-
-                var hwnd = User32.FindWindow(_className, null);
-
-                appbar.cbSize = Marshal.SizeOf(appbar);
-                appbar.uEdge = 0;
-                appbar.hWnd = hwnd;
-                appbar.lParam = 1;
-
-                User32.GetWindowRect(hwnd, out RECT scaledTaskbarRect);
-
-                var taskbarNonDPIAwareSize = Shell32.SHAppBarMessage(Shell32.ABMsg.ABM_GETTASKBARPOS, ref appbar);
-                var scalingAmount = (double)(scaledTaskbarRect.Bottom - scaledTaskbarRect.Top) / (appbar.rc.Bottom - appbar.rc.Top);
-
-                State retState = new State();
-                retState.Size.Top = (int)(appbar.rc.Top * scalingAmount);
-                retState.Size.Bottom = (int)(appbar.rc.Bottom * scalingAmount);
-                retState.Size.Left = (int)(appbar.rc.Left * scalingAmount);
-                retState.Size.Right = (int)(appbar.rc.Right * scalingAmount);
+                var state = new State();
+                var hwnd = User32.FindWindow("Shell_TrayWnd", null);
+                User32.GetWindowRect(hwnd, out state.Size);
 
                 var screen = Screen.AllScreens.FirstOrDefault(x => x.Bounds.Contains(
                     new Rectangle(
-                        retState.Size.Left,
-                        retState.Size.Top,
-                        retState.Size.Right - retState.Size.Left,
-                        retState.Size.Bottom - retState.Size.Top)
+                        state.Size.Left,
+                        state.Size.Top,
+                        state.Size.Right - state.Size.Left,
+                        state.Size.Bottom - state.Size.Top)
                 ));
 
-                retState.ContainingScreen = screen;
-                retState.Location = Position.Bottom;
+                state.ContainingScreen = screen;
+                state.Location = Position.Bottom;
 
                 if (screen != null)
                 {
-                    if (retState.Size.Bottom == screen.Bounds.Bottom && retState.Size.Top == screen.Bounds.Top)
+                    if (state.Size.Bottom == screen.Bounds.Bottom && state.Size.Top == screen.Bounds.Top)
                     {
-                        retState.Location = (retState.Size.Left == screen.Bounds.Left) ? Position.Left : Position.Right;
+                        state.Location = (state.Size.Left == screen.Bounds.Left) ? Position.Left : Position.Right;
                     }
-                    if (retState.Size.Right == screen.Bounds.Right && retState.Size.Left == screen.Bounds.Left)
+                    if (state.Size.Right == screen.Bounds.Right && state.Size.Left == screen.Bounds.Left)
                     {
-                        retState.Location = (retState.Size.Top == screen.Bounds.Top) ? Position.Top : Position.Bottom;
+                        state.Location = (state.Size.Top == screen.Bounds.Top) ? Position.Top : Position.Bottom;
                     }
                 }
 
-                return retState;
+                return state;
             }
         }
     }
-
-
 }
