c226c580b34053a10dbaaa789f0eb5e34f9bfc00
Amenta <dave@daveamenta.com>
2018-06-05 19:58:17 -0700

    Generalize OpacityConverter

--- EarTrumpet/Views/InvertableVisibilityConverter.cs
+++ EarTrumpet/Views/InvertableVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace EarTrumpet.Extensions
+namespace EarTrumpet.Views
 {
     [ValueConversion(typeof(bool), typeof(Visibility))]
     public class InvertableVisibilityConverter : IValueConverter
