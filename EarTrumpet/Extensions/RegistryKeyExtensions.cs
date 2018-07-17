129be58744599e70ce64ce77363bf78cf6853a0f
Dave Amenta <dave@daveamenta.com>
2018-07-17 00:24:18 -0700

    Fix subtle bug in ReadPersonalizationSetting

---
+++ EarTrumpet/Extensions/RegistryKeyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+
+namespace EarTrumpet.Extensions
+{
+    public static class RegistryKeyExtensions
+    {
+        public static T GetValue<T>(this RegistryKey self, string valueName, T defaultValue)
+        {
+            // Manually apply defaultValue if the data type is mismatched.
+            var value = self.GetValue(valueName, defaultValue);
+            if (value is T)
+            {
+                return (T)value;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
