a261a3760267f09c8973076f6ab09c059669b50b
Dave Amenta <dave@daveamenta.com>
2018-07-21 08:31:24 -0700

    Tweaks to addon settings window

--- EarTrumpet/UI/Views/AddonSettingsWindow.xaml.cs
+++ EarTrumpet/UI/Views/AddonSettingsWindow.xaml.cs
@@ -1,25 +1,43 @@
 ﻿using EarTrumpet.DataModel;
 using EarTrumpet.Extensibility;
+using EarTrumpet.Extensions;
 using EarTrumpet.Interop.Helpers;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace EarTrumpet.UI.Views
 {
     public partial class AddonSettingsWindow : Window
     {
+        private static Dictionary<ISettingsEntry, AddonSettingsWindow> s_windows = new Dictionary<ISettingsEntry, AddonSettingsWindow>();
+
         private readonly ISettingsEntry _addon;
         public AddonSettingsWindow(ISettingsEntry addon)
         {
             _addon = addon;
             InitializeComponent();
 
-            Title = $"EarTrumpet - {addon.DisplayName}";
+            Title = addon.DisplayName;
 
             AddonHostGrid.Children.Add((UIElement)addon.Content);
 
             SourceInitialized += (_, __) => AccentPolicyLibrary.SetWindowBlur(this, true, true);
 
             this.FlowDirection = SystemSettings.IsRTL ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+
+            Closing += AddonSettingsWindow_Closing;
+        }
+
+        private void AddonSettingsWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (var pair in s_windows)
+            {
+                if (pair.Value == this)
+                {
+                    s_windows.Remove(pair.Key);
+                    break;
+                }
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -29,10 +47,16 @@ namespace EarTrumpet.UI.Views
 
         public static void ShowForAddon(ISettingsEntry addon)
         {
-            var win = new AddonSettingsWindow(addon);
-            win.Show();
-
-            // TODO: dictionary and single-instance behavior
+            if (s_windows.ContainsKey(addon))
+            {
+                s_windows[addon].RaiseWindow();
+            }
+            else
+            {
+                var win = new AddonSettingsWindow(addon);
+                win.Show();
+                s_windows.Add(addon, win);
+            }
         }
     }
 }
