a8e6d0a37948cb48283188a79daccb575583629e
WhAtEvErYoUmEaN <WhAtEvErYoUmEaN@users.noreply.github.com>
2019-02-03 15:11:22 +0100

    Possible fix for #273
    I couldn't get it to build on my machine due to missing dependencies which i have no time to hunt after, but this should clear all possible leftover handles and stop EarTrumpet from "holding other apps hostage"

--- EarTrumpet/Interop/Helpers/IconUtils.cs
+++ EarTrumpet/Interop/Helpers/IconUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Windows;
@@ -15,31 +15,35 @@ namespace EarTrumpet.Interop.Helpers
             var moduleHandle = Kernel32.LoadLibraryEx(path, IntPtr.Zero,
                 Kernel32.LoadLibraryFlags.LOAD_LIBRARY_AS_DATAFILE | Kernel32.LoadLibraryFlags.LOAD_LIBRARY_AS_IMAGE_RESOURCE);
 
+            Icon returnIcon;
             IntPtr iconHandle = IntPtr.Zero;
             try
             {
                 Comctl32.LoadIconMetric(moduleHandle, new IntPtr(iconOrdinal), useLargeIcon ? Comctl32.LI_METRIC.LIM_LARGE : Comctl32.LI_METRIC.LIM_SMALL, ref iconHandle);
+                returnIcon = (Icon)Icon.FromHandle(iconHandle).Clone();
             }
             finally
             {
                 Kernel32.FreeLibrary(moduleHandle);
+                User32.DestroyIcon(iconHandle);
             }
-
-            return Icon.FromHandle(iconHandle);
+            return returnIcon;
         }
 
         public static ImageSource GetIconAsImageSourceFromFile(string path, int iconIndex = 0)
         {
+            ImageSource returnImageSource;
             IntPtr iconHandle = IntPtr.Zero;
             try
             {
                 iconHandle = Shell32.ExtractIcon(Process.GetCurrentProcess().Handle, path, iconIndex);
-                return Imaging.CreateBitmapSourceFromHIcon(iconHandle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                returnImageSource = Imaging.CreateBitmapSourceFromHIcon(iconHandle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             }
             finally
             {
                 User32.DestroyIcon(iconHandle);
             }
+            return returnImageSource;
         }
     }
 }
