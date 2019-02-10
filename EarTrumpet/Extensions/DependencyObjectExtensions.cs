a960150218767d71c52cd86748ee0e5a5e50db53
Dave Amenta <dave@daveamenta.com>
2019-02-09 20:07:38 -0800

    Tooltips

--- EarTrumpet/Extensions/DependencyObjectExtensions.cs
+++ EarTrumpet/Extensions/DependencyObjectExtensions.cs
@@ -5,7 +5,50 @@ namespace EarTrumpet.Extensions
 {
     public static class DependencyObjectExtensions
     {
-        public static  childItem FindVisualChild<childItem>(this DependencyObject obj) where childItem : DependencyObject
+        public static T FindVisualParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            var parentObject = GetParentObject(obj);
+            if (parentObject == null)
+            {
+                return null;
+            }
+
+            return (parentObject is T) ? (T)parentObject : FindVisualParent<T>(parentObject);
+        }
+
+        public static DependencyObject GetParentObject(this DependencyObject child)
+        {
+            if (child == null)
+            {
+                return null;
+            }
+
+            var contentElement = child as ContentElement;
+            if (contentElement != null)
+            {
+                var parent = ContentOperations.GetParent(contentElement);
+                if (parent != null)
+                {
+                    return parent;
+                }
+
+                var fce = contentElement as FrameworkContentElement;
+                return fce != null ? fce.Parent : null;
+            }
+
+            var frameworkElement = child as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                var parent = frameworkElement.Parent;
+                if (parent != null)
+                {
+                    return parent;
+                }
+            }
+            return VisualTreeHelper.GetParent(child);
+        }
+
+        public static childItem FindVisualChild<childItem>(this DependencyObject obj) where childItem : DependencyObject
         {
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
             {
