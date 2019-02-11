e40dab3939f4788d68a491c3d559b2ac6ae44f5c
Dave Amenta <dave@daveamenta.com>
2019-02-11 06:34:30 -0800

    Reset scrollviewer on context change

---
+++ EarTrumpet/UI/Behaviors/ScrollViewerEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EarTrumpet.UI.Behaviors
+{
+    public static class ScrollViewerEx
+    {
+        public static object GetScrollToVerticalOffset(DependencyObject obj) => (object)obj.GetValue(ScrollToVerticalOffsetProperty);
+        public static void SetScrollToVerticalOffset(DependencyObject obj, object value) => obj.SetValue(ScrollToVerticalOffsetProperty, value);
+        public static readonly DependencyProperty ScrollToVerticalOffsetProperty =
+        DependencyProperty.RegisterAttached("ScrollToVerticalOffset", typeof(object), typeof(ScrollViewerEx), new PropertyMetadata(null, ScrollToVerticalOffsetChanged));
+        private static void ScrollToVerticalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((ScrollViewer)d).ScrollToVerticalOffset(0);
+        }
+
+    }
+}
