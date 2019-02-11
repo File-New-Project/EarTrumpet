116290e88a36cddc3711d99dbf137169496791b5
Dave Amenta <dave@daveamenta.com>
2019-02-10 19:55:43 -0800

    Make AdvertisedCategorySettingsViewModel public

--- EarTrumpet/UI/ViewModels/AdvertisedCategorySettingsViewModel.cs
+++ EarTrumpet/UI/ViewModels/AdvertisedCategorySettingsViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace EarTrumpet.UI.ViewModels
 {
-    class AdvertisedCategorySettingsViewModel : SettingsCategoryViewModel
+    public class AdvertisedCategorySettingsViewModel : SettingsCategoryViewModel
     {
         string _link;
 
