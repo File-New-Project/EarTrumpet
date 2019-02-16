9f3e1a60db35c0a14594c5ca26d7d0e468811069
Dave Amenta <dave@daveamenta.com>
2019-02-15 19:08:09 -0800

    Update version from a single source

--- EarTrumpet/prebuild.ps1
+++ EarTrumpet/prebuild.ps1
@@ -1,3 +1,29 @@
 ﻿cd $PSScriptRoot
-$xml = [xml](Get-Content ..\EarTrumpet.UWP\Package.appxmanifest)
-Set-Content .\Assets\DevVersion.txt ($xml.Package.Identity.Version)
+
+function UpdateManifest { param($path, $version)
+	$xml = [xml](Get-Content $path)
+	$xml.Package.Identity.Version = "$version"
+	$xml.Save((Resolve-Path ("$PSScriptRoot\$path")))
+}
+
+$UWPManifestPath = "..\EarTrumpet.UWP\Package.appxmanifest"
+$PackageManifestPath = "..\EarTrumpet.Package\Package.appxmanifest"
+$AssemblyInfoPath = ".\Properties\AssemblyInfo.cs"
+
+$newVersion = Get-Content -raw ..\version.txt
+
+UpdateManifest -path $UWPManifestPath -version $newVersion
+UpdateManifest -path $PackageManifestPath -version $newVersion
+
+$assemblyInfo = Get-Content -raw $AssemblyInfoPath
+
+if ($assemblyInfo -match ".*?Version\(`"(.*?)`"\).*?") {
+	$assemblyInfo = $assemblyInfo.Replace($Matches[1], $newVersion);
+	Set-Content -Path $AssemblyInfoPath -Value $assemblyInfo
+} else {
+	throw "Unexpected lack of AssemblyInfo.cs match when searching for version";
+}
+
+Set-Content .\Assets\DevVersion.txt $newVersion
+
+write-host "Version: $newVersion from version.txt"
