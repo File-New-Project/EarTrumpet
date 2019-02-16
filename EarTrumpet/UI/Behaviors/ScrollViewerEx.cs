4738a1e578a810b76eed9dd48adb69d5947d0cd3
Dave Amenta <dave@daveamenta.com>
2019-02-15 18:15:13 -0800

    Rename

--- EarTrumpet/UI/Behaviors/ScrollViewerEx.cs
+++ EarTrumpet/UI/Behaviors/ScrollViewerEx.cs
@@ -5,13 +5,13 @@ namespace EarTrumpet.UI.Behaviors
 {
     public static class ScrollViewerEx
     {
-        public static object GetScrollToVerticalOffset(DependencyObject obj) => (object)obj.GetValue(ScrollToVerticalOffsetProperty);
-        public static void SetScrollToVerticalOffset(DependencyObject obj, object value) => obj.SetValue(ScrollToVerticalOffsetProperty, value);
-        public static readonly DependencyProperty ScrollToVerticalOffsetProperty =
-        DependencyProperty.RegisterAttached("ScrollToVerticalOffset", typeof(object), typeof(ScrollViewerEx), new PropertyMetadata(null, ScrollToVerticalOffsetChanged));
-        private static void ScrollToVerticalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public static object GetScrollToTopOnChanged(DependencyObject obj) => (object)obj.GetValue(ScrollToTopOnChangedProperty);
+        public static void SetScrollToTopOnChanged(DependencyObject obj, object value) => obj.SetValue(ScrollToTopOnChangedProperty, value);
+        public static readonly DependencyProperty ScrollToTopOnChangedProperty =
+        DependencyProperty.RegisterAttached("ScrollToTopOnChanged", typeof(object), typeof(ScrollViewerEx), new PropertyMetadata(null, ScrollToTopOnChanged));
+        private static void ScrollToTopOnChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
-            ((ScrollViewer)d).ScrollToVerticalOffset(0);
+            ((ScrollViewer)dependencyObject).ScrollToVerticalOffset(0);
         }
 
     }
