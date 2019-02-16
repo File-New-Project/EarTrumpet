0b4d56481e8235062e88f6d091dceaf0cbad6e36
Dave Amenta <dave@daveamenta.com>
2019-02-15 23:21:18 -0800

    Remove ad

--- EarTrumpet.Actions/SettingsPageAddon.cs
+++ EarTrumpet.Actions/SettingsPageAddon.cs
@@ -15,16 +15,4 @@ namespace EarTrumpet.Actions
             return new ActionsCategoryViewModel();
         }
     }
-
-#if DEBUG
-    [Export(typeof(IAddonSettingsPage))]
-    class SettingsPageAddonAd : IAddonSettingsPage
-    {
-        public SettingsCategoryViewModel Get()
-        {
-            return new AdvertisedCategorySettingsViewModel(
-                Properties.Resources.MyActionsText, "\xE950", Properties.Resources.AddonDescriptionText, "EarTrumpet.project-eta-adonly", "https://github.com/File-New-Project/EarTrumpet");
-        }
-    }
-#endif
 }
