819ddb44f650dbc12f5afc8145bbf74d82e8c475
Dave Amenta <dave@daveamenta.com>
2019-02-26 22:11:15 -0800

    Tray ext and cleanup

--- EarTrumpet/UI/Tray/ITrayViewModel.cs
+++ EarTrumpet/UI/Tray/ITrayViewModel.cs
@@ -1,16 +1,17 @@
-﻿using EarTrumpet.UI.Helpers;
-using EarTrumpet.UI.ViewModels;
+﻿using EarTrumpet.UI.ViewModels;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Windows.Input;
 
 namespace EarTrumpet.UI.Tray
 {
     public interface ITrayViewModel : INotifyPropertyChanged
     {
-        RelayCommand LeftClick { get; }
+        ICommand LeftClick { get; set; }
+        ICommand MiddleClick { get; }
         IEnumerable<ContextMenuItem> MenuItems { get; }
-        RelayCommand MiddleClick { get; }
+        ICommand OpenMixer { get; set; }
         string ToolTip { get; }
         Icon TrayIcon { get; }
         void Refresh();
