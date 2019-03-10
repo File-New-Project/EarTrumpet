9be8ec59e9e453d485c6a0bcf88b6d451dfffa45
Dave Amenta <dave@daveamenta.com>
2019-03-09 17:34:13 -0800

    Reduce duplication in HandledBindingExtension

--- EarTrumpet/Extensions/EventBinding/HandledEventBindingExtension.cs
+++ EarTrumpet/Extensions/EventBinding/HandledEventBindingExtension.cs
@@ -1,37 +1,9 @@
-﻿using System;
-using System.Reflection;
-using System.Windows.Markup;
-
-namespace EarTrumpet.Extensions.EventBinding
+﻿namespace EarTrumpet.Extensions.EventBinding
 {
     // {Event:HandledBinding}
-    public class HandledBindingExtension : MarkupExtension
+    public class HandledBindingExtension : BindingExtension
     {
-        public HandledBindingExtension() { }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            var provideValueTarget = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
-            if (provideValueTarget != null)
-            {
-                Type delegateType = null;
-                if ((provideValueTarget.TargetProperty as EventInfo) != null)
-                {
-                    delegateType = (provideValueTarget.TargetProperty as EventInfo).EventHandlerType;
-                }
-                else
-                {
-                    delegateType = (provideValueTarget.TargetProperty as MethodInfo).GetParameters()[1].ParameterType;
-                }
-
-                return Delegate.CreateDelegate(delegateType,
-                    this,
-                    GetType().GetMethod(nameof(OnEvent), BindingFlags.NonPublic | BindingFlags.Instance));
-            }
-            return null;
-        }
-
-        void OnEvent(object sender, object args)
+        protected override void OnEvent(object sender, object args)
         {
             args.GetType().GetProperty("Handled").SetValue(args, true);
         }
