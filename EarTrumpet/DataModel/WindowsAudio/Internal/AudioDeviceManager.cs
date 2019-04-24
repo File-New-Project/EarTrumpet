6a35d97300ae1a5b97f896c452ffbc9d4a3d83e6
Dave Amenta <dave@daveamenta.com>
2019-04-24 11:05:15 -0700

    Prevent sending apps to an invalid persisted endpoint

--- EarTrumpet/DataModel/WindowsAudio/Internal/AudioDeviceManager.cs
+++ EarTrumpet/DataModel/WindowsAudio/Internal/AudioDeviceManager.cs
@@ -275,7 +275,13 @@ namespace EarTrumpet.DataModel.WindowsAudio.Internal
 
         public void SetDefaultEndPoint(string id, int pid)
         {
-            _policyConfigService.SetDefaultEndPoint(id, pid);
+            // Note: We found it unexpected that SetDefaultEndPoint
+            // would accept an id for an 'invalid' device and then 
+            // no audio will be heard from any device for the given pid.
+            if (id == null || TryFind(id, out _))
+            {
+                _policyConfigService.SetDefaultEndPoint(id, pid);
+            }
         }
 
         public string GetDefaultEndPoint(int processId)
