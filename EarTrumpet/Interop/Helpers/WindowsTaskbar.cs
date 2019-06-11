403a24f4ec4193c3f2548cff4de0692d2474d7c0
Dave Amenta <dave@daveamenta.com>
2019-06-11 07:00:49 -0700

    Avoid a rare SEHException during Screen.AllScreens call

--- EarTrumpet/Interop/Helpers/WindowsTaskbar.cs
+++ EarTrumpet/Interop/Helpers/WindowsTaskbar.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -25,42 +23,37 @@ namespace EarTrumpet.Interop.Helpers
             Bottom = 3
         }
 
-        public static uint Dpi
-        {
-            get
-            {
-                var hWnd = User32.FindWindow("Shell_TrayWnd", null);
-                return User32.GetDpiForWindow(hWnd);
-            }
-        }
+        public static uint Dpi => User32.GetDpiForWindow(GetHwnd());
 
         public static State Current
         {
             get
             {
-                var hWnd = User32.FindWindow("Shell_TrayWnd", null);
-                var state = new State();
+                var hWnd = GetHwnd();
+                var state = new State
+                {
+                    ContainingScreen = Screen.FromHandle(hWnd),
+                };
                 var appBarData = new APPBARDATA
                 {
                     cbSize = (uint)Marshal.SizeOf(typeof(APPBARDATA)),
                     hWnd = hWnd
                 };
+
                 // SHAppBarMessage: Understands Taskbar auto-hide
                 // state (the window is positioned across screens).
                 if (Shell32.SHAppBarMessage(AppBarMessage.GetTaskbarPos, ref appBarData) != UIntPtr.Zero)
                 {
                     state.Size = appBarData.rect;
                     state.Location = (Position)appBarData.uEdge;
-                    state.ContainingScreen = GetScreenFromRect(state.Size);
                 }
                 else
                 {
                     User32.GetWindowRect(hWnd, out state.Size);
-                    var screen = GetScreenFromRect(state.Size);
-                    state.ContainingScreen = screen;
 
-                    if (screen != null)
+                    if (state.ContainingScreen != null)
                     {
+                        var screen = state.ContainingScreen;
                         if (state.Size.Bottom == screen.Bounds.Bottom && state.Size.Top == screen.Bounds.Top)
                         {
                             state.Location = (state.Size.Left == screen.Bounds.Left) ? Position.Left : Position.Right;
@@ -79,15 +72,6 @@ namespace EarTrumpet.Interop.Helpers
             }
         }
 
-        private static Screen GetScreenFromRect(RECT rect)
-        {
-            return Screen.AllScreens.FirstOrDefault(x => x.Bounds.Contains(
-                new Rectangle(
-                rect.Left,
-                rect.Top,
-                rect.Right - rect.Left,
-                rect.Bottom - rect.Top)
-            ));
-        }
+        private static IntPtr GetHwnd() => User32.FindWindow("Shell_TrayWnd", null);
     }
 }
