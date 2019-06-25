9abd600c2f65c017291033e3689d6b9ec5355459
Rafael Rivera <rafael@withinwindows.com>
2019-06-25 00:51:11 -0700

    Set up Azure Pipeline

--- EarTrumpet/Properties/AssemblyInfo.cs
+++ EarTrumpet/Properties/AssemblyInfo.cs
@@ -1,16 +1,7 @@
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("EarTrumpet")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("File-New-Project")]
 [assembly: AssemblyProduct("EarTrumpet")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.1.5.0")]
-[assembly: AssemblyFileVersion("2.1.5.0")]
-[assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
--- EarTrumpet/prebuild.ps1
+++ EarTrumpet/prebuild.ps1
@@ -1,21 +1,7 @@
-﻿cd $PSScriptRoot
+﻿param($version)
 
-# Goal: Keep all versions in sync with the master version.txt
-$newVersion = Get-Content -raw ..\version.txt
-write-host "Version: $newVersion from version.txt"
+$PackageManifestPath = "$PSScriptRoot\..\EarTrumpet.Package\Package.appxmanifest"
 
-# Update Package.appxmanifest
-$PackageManifestPath = "..\EarTrumpet.Package\Package.appxmanifest"
 $xml = [xml](Get-Content $PackageManifestPath)
-$xml.Package.Identity.Version = "$newVersion"
-$xml.Save((Resolve-Path ("$PSScriptRoot\$PackageManifestPath")))
-
-# Update AssemblyInfo.cs
-$AssemblyInfoPath = ".\Properties\AssemblyInfo.cs"
-$assemblyInfo = Get-Content -raw $AssemblyInfoPath
-if ($assemblyInfo -match ".*?Version\(`"(.*?)`"\).*?") {
-	$assemblyInfo = $assemblyInfo.Replace($Matches[1], $newVersion);
-	Set-Content -Path $AssemblyInfoPath -Value $assemblyInfo.Trim();
-} else {
-	throw "Unexpected lack of AssemblyInfo.cs match when searching for version";
-}
+$xml.Package.Identity.Version = "$version"
+$xml.Save($PackageManifestPath)
