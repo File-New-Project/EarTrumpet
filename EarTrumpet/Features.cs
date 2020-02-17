f80d1fd0d7616e2f3fac28868a31553e26edde41
Rafael Rivera <rafael@withinwindows.com>
2020-02-16 17:15:24 -0800

    Added loc text for GDPR controls

--- EarTrumpet/Features.cs
+++ EarTrumpet/Features.cs
@@ -2,35 +2,5 @@
 {
     public class Features
     {
-        // Reason:
-        //
-        // Localization required: ToggleMuteToolTip
-        public static bool IsDeviceMuteToolTipEnabled
-        {
-            get
-            {
-#if DEBUG
-                return true;
-#else
-                return false;
-#endif
-            }
-        }
-
-        // Reason:
-        //
-        // Localization required: PrivacyCheckboxText
-        // Localization required: PrivacyLinkText
-        public static bool IsTelemetryConfigurable
-        {
-            get
-            {
-#if DEBUG
-                return true;
-#else
-                return false;
-#endif
-            }
-        }
     }
 }
--- EarTrumpet/Properties/Resources.Designer.cs
+++ EarTrumpet/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace EarTrumpet.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -369,20 +369,20 @@ namespace EarTrumpet.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Privacy policy.
-        /// </summary>
-        public static string PrivacyPolicyText {
-            get {
-                return ResourceManager.GetString("PrivacyPolicyText", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Send crash data to the EarTrumpet team.
         /// </summary>
         public static string PrivacyCheckboxText {
             get {
                 return ResourceManager.GetString("PrivacyCheckboxText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Privacy policy.
+        /// </summary>
+        public static string PrivacyPolicyText {
+            get {
+                return ResourceManager.GetString("PrivacyPolicyText", resourceCulture);
             }
         }
         
