d4007f9d0bd21dbcf748e6c91ffaf5360d75039d
Rafael Rivera <rafael@withinwindows.com>
2020-04-05 14:14:19 -0700

    Add Nuget package

---
+++ .chocolatey/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,4 @@
+﻿$process = Get-Process -Name EarTrumpet -ErrorAction SilentlyContinue
+if ($process) {
+  $process | Stop-Process -Force
+}
---
+++ .chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installPath = Join-Path $toolsDir 'EarTrumpet'
+$exePath = Join-Path $toolsDir 'EarTrumpet\EarTrumpet.exe'
+$zipPath = Join-Path $toolsDir 'Release.zip'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$zipPath" `
+                             -UnzipLocation "$installPath"
+                             
+Write-Output "Adding shortcut to Start Menu"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\EarTrumpet.lnk" -TargetPath $exePath
+
+Write-Output "Adding shortcut to Startup"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\EarTrumpet.lnk" -TargetPath $exePath
