b57ab7efc910a846d334d067b7711d697edee74b
svenwml <svenwml@yahoo.de>
2021-01-16 01:52:39 +0100

    add hardware control configuration elements, remove unused methods

--- EarTrumpet/UI/ViewModels/EarTrumpetHardwareControlsPageViewModel.cs
+++ EarTrumpet/UI/ViewModels/EarTrumpetHardwareControlsPageViewModel.cs
@@ -1,18 +1,14 @@
 ﻿using EarTrumpet.Interop.Helpers;
 using EarTrumpet.UI.Helpers;
 using System;
-using System.Diagnostics;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace EarTrumpet.UI.ViewModels
 {
     class EarTrumpetHardwareControlsPageViewModel : SettingsPageViewModel
     {
-        public ICommand OpenDiagnosticsCommand { get; }
-        public ICommand OpenAboutCommand { get; }
-        public ICommand OpenFeedbackCommand { get; }
-        public ICommand OpenPrivacyPolicyCommand { get; }
-        public string AboutText { get; }
+        public ICommand AddMidiControlCommand { get; }
 
         public bool IsTelemetryEnabled
         {
@@ -20,36 +16,28 @@ namespace EarTrumpet.UI.ViewModels
             set => _settings.IsTelemetryEnabled = value;
         }
 
-        private readonly Action _openDiagnostics;
         private readonly AppSettings _settings;
 
-        public EarTrumpetHardwareControlsPageViewModel(Action openDiagnostics, AppSettings settings) : base(null)
+        public EarTrumpetHardwareControlsPageViewModel(AppSettings settings) : base(null)
         {
             _settings = settings;
-            _openDiagnostics = openDiagnostics;
             Glyph = "\xF8A6";
             Title = Properties.Resources.HardwareControlsTitle;
-            AboutText = $"EarTrumpet {App.PackageVersion}";
 
-            OpenAboutCommand = new RelayCommand(OpenAbout);
-            OpenDiagnosticsCommand = new RelayCommand(OpenDiagnostics);
-            OpenFeedbackCommand = new RelayCommand(OpenFeedbackHub);
-            OpenPrivacyPolicyCommand = new RelayCommand(OpenPrivacyPolicy);
+            AddMidiControlCommand = new RelayCommand(AddMidiControl);
         }
-
-        private void OpenDiagnostics()
+        public ObservableCollection<string> HardwareControls
         {
-            if (Keyboard.IsKeyDown(Key.LeftShift) && Keyboard.IsKeyDown(Key.LeftCtrl))
+            get
             {
-                Trace.WriteLine($"EarTrumpetAboutPageViewModel OpenDiagnostics - CRASH");
-                throw new Exception("This is an intentional crash.");
+                ObservableCollection<String> a = new ObservableCollection<string>();
+                a.Add("Sample 1");
+                a.Add("Sample 2");
+                return a;
             }
-
-            _openDiagnostics.Invoke();
         }
 
-        private void OpenFeedbackHub() => ProcessHelper.StartNoThrow("windows-feedback:///?appid=40459File-New-Project.EarTrumpet_1sdd7yawvg6ne!EarTrumpet");
-        private void OpenAbout() => ProcessHelper.StartNoThrow("https://github.com/File-New-Project/EarTrumpet");
-        private void OpenPrivacyPolicy() => ProcessHelper.StartNoThrow("https://github.com/File-New-Project/EarTrumpet/blob/master/PRIVACY.md");
+        // ToDo: Open window for MIDI control selection.
+        private void AddMidiControl() => ProcessHelper.StartNoThrow("https://github.com/File-New-Project/EarTrumpet");
     }
 }
