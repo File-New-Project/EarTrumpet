f06cab135c8778f68e0505d217907e443d6514b5
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2021-01-16 17:24:11 +0100

    Added classes for finding MIDI devices and listening to their 'control change' messages

---
+++ EarTrumpet/DataModel/MIDI/MidiIn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Midi;
+using System.Threading.Tasks;
+
+namespace EarTrumpet.DataModel.MIDI
+{
+    public class MidiIn
+    {
+        private static async Task<List<MidiInDevice>> _GetAllDevices(bool returnEmptyNames=false)
+        {
+            var midiInputQueryString = MidiInPort.GetDeviceSelector();
+            var midiInputDevices = await DeviceInformation.FindAllAsync(midiInputQueryString);
+
+            return (from device in midiInputDevices where returnEmptyNames || device.Name.Length != 0 select new MidiInDevice(device)).ToList();
+        }
+
+        public static List<MidiInDevice> GetAllDevices(bool returnEmptyNames=false)
+        {
+            return _GetAllDevices(returnEmptyNames).Result;
+        }
+
+        public static MidiInDevice GetDeviceByName(string name)
+        {
+            return GetAllDevices().FirstOrDefault(device => device.Name == name);
+        }
+        
+        public static MidiInDevice GetDeviceById(string id)
+        {
+            return GetAllDevices().FirstOrDefault(device => device.Id == id);
+        }
+    }
+}
