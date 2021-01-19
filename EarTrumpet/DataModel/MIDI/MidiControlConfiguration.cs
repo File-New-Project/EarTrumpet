1c365242a61ea9dd5739fd6de53eaa7afa633ad6
svenwml <svenwml@yahoo.de>
2021-01-19 22:23:44 +0100

    add midi control configuration class

---
+++ EarTrumpet/DataModel/MIDI/MidiControlConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace EarTrumpet.DataModel.MIDI
+{
+    public class MidiControlConfiguration
+    {
+        byte Channel { get; set; }
+        byte Controller { get; set; }
+        byte MinValue { get; set; }
+        byte MaxValue { get; set; }
+        float ScalingValue { get; set; }
+
+        // Constructor
+        public MidiControlConfiguration(byte Channel, byte Controller, byte MinValue, byte MaxValue, float ScalingValue)
+        {
+            this.Channel = Channel;
+            this.Controller = Controller;
+            this.MinValue = MinValue;
+            this.MaxValue = MaxValue;
+            this.ScalingValue = ScalingValue;
+        }
+    }
+}
