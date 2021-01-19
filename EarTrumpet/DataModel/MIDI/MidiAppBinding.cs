35a56e9aafe862361283d775a833fc6f4463e681
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2021-01-20 00:32:19 +0100

    The MidiAppBinding now supports all types of commands

--- EarTrumpet/DataModel/MIDI/MidiAppBinding.cs
+++ EarTrumpet/DataModel/MIDI/MidiAppBinding.cs
@@ -27,43 +27,140 @@ namespace EarTrumpet.DataModel.MIDI
 
             return (device?.Apps).FirstOrDefault(app => app.DisplayName == appName);
         }
+        
+        private IAppItemViewModel GetAppByIndex(string deviceName, string index)
+        {
+            var device = GetDeviceByName(deviceName);
+            int i;
 
+            try
+            {
+                i = Int32.Parse(index);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            
+            if (device.Apps.Count() < i)
+            {
+                return null;
+            }
+
+            return device.Apps[i];
+        }
+
+        private bool MidiEquals(MidiControlConfiguration midiConfig, MidiControlChangeMessage msg)
+        {
+            return midiConfig.Channel == msg.Channel && midiConfig.Controller == msg.Controller;
+        }
+
+        private int ToVolume(MidiControlConfiguration midiConfig, MidiControlChangeMessage msg)
+        {
+            int newVolume;
+            float fullScaleRange = (float) midiConfig.MaxValue - midiConfig.MinValue;
+
+            // Division by zero is not allowed.
+            // -> Set minimum full scale range in these cases.
+            if(fullScaleRange == 0)
+            {
+                fullScaleRange = 1;
+            }
+                        
+            if(midiConfig.MaxValue > midiConfig.MinValue)
+            {
+                newVolume = Math.Abs((int)(((msg.ControlValue - midiConfig.MinValue) / (float)fullScaleRange) * midiConfig.ScalingValue * 100.0));
+            }
+            else
+            {
+                newVolume = 100 - Math.Abs((int)(((msg.ControlValue - midiConfig.MaxValue) / (float)fullScaleRange) * midiConfig.ScalingValue * 100.0));
+            }
+
+            return newVolume;
+        }
+        private void ApplicationVolume(CommandControlMappingElement command, MidiControlChangeMessage msg)
+        {
+            IAppItemViewModel app = null;
+            if (command.mode == "Application Selection")
+            {
+                app = GetAppByName(command.audioDevice, command.indexApplicationSelection);
+            } else if (command.mode == "Indexed")
+            {
+                app = GetAppByIndex(command.audioDevice, command.indexApplicationSelection);
+            }
+                
+            if (app == null)
+            {
+                return;
+            }
+
+            app.Volume = ToVolume(command.midiControlConfiguration, msg);
+        }
+
+        private void ApplicationMute(CommandControlMappingElement command, MidiControlChangeMessage msg)
+        {
+            IAppItemViewModel app = null;
+            if (command.mode == "Application Selection")
+            {
+                app = GetAppByName(command.audioDevice, command.indexApplicationSelection);
+            } else if (command.mode == "Indexed")
+            {
+                app = GetAppByIndex(command.audioDevice, command.indexApplicationSelection);
+            }
+                
+            if (app == null)
+            {
+                return;
+            }
+            
+            int volume = ToVolume(command.midiControlConfiguration, msg);
+            app.IsMuted = (volume < 50);
+        }
+
+        private void SystemVolume(CommandControlMappingElement command, MidiControlChangeMessage msg)
+        {
+            var device = GetDeviceByName(command.audioDevice);
+            if (device == null)
+            {
+                return;
+            }
+
+            device.Volume = ToVolume(command.midiControlConfiguration, msg);
+        }
+
+        private void SystemMute(CommandControlMappingElement command, MidiControlChangeMessage msg)
+        {
+            var device = GetDeviceByName(command.audioDevice);
+            if (device == null)
+            {
+                return;
+            }
+
+            int volume = ToVolume(command.midiControlConfiguration, msg);
+            device.IsMuted = (volume < 50);
+        }
+        
         private void MidiCallback(MidiInPort sender, MidiControlChangeMessage msg)
         {
             foreach (var command in _commandControlMappings)
             {
                 if (command.midiDevice == MidiIn.GetDeviceById(sender.DeviceId).Name)
                 {
-                    var midiConfig = command.midiControlConfiguration;
-
-                    if (msg.Channel == midiConfig.Channel && msg.Controller == midiConfig.Controller)
+                    if (MidiEquals(command.midiControlConfiguration, msg))
                     {
-                        var app = GetAppByName(command.audioDevice, command.indexApplicationSelection);
-                        if (app == null)
+                        if (command.command == "System Volume")
                         {
-                            continue;
-                        }
-                        
-                        int newVolume;
-                        float fullScaleRange = (float) midiConfig.MaxValue - midiConfig.MinValue;
-
-                        // Division by zero is not allowed.
-                        // -> Set minimum full scale range in these cases.
-                        if(fullScaleRange == 0)
+                            SystemVolume(command, msg);
+                        } else if (command.command == "System Mute")
                         {
-                            fullScaleRange = 1;
-                        }
-                        
-                        if(midiConfig.MaxValue > midiConfig.MinValue)
+                            SystemMute(command, msg);
+                        } else if (command.command == "Application Volume")
                         {
-                            newVolume = Math.Abs((int)(((msg.ControlValue - midiConfig.MinValue) / (float)fullScaleRange) * midiConfig.ScalingValue * 100.0));
-                        }
-                        else
+                            ApplicationVolume(command, msg);
+                        } else if (command.command == "Application Mute")
                         {
-                            newVolume = 100 - Math.Abs((int)(((msg.ControlValue - midiConfig.MaxValue) / (float)fullScaleRange) * midiConfig.ScalingValue * 100.0));
+                            ApplicationMute(command, msg);
                         }
-
-                        app.Volume = newVolume;
                     }
                 }
             }
@@ -71,7 +168,19 @@ namespace EarTrumpet.DataModel.MIDI
 
         public void AddCommand(CommandControlMappingElement command)
         {
+            MidiIn._StartListening(MidiIn.GetDeviceByName(command.midiDevice).Id);
+            
             _commandControlMappings.Add(command);
+        }
+
+        public void RemoveCommandIndex(int index)
+        {
+            if (_commandControlMappings.Count < index)
+            {
+                return;
+            }
+            
+            _commandControlMappings.RemoveAt(index);
         }
         
         public MidiAppBinding(DeviceCollectionViewModel deviceCollectionViewModel)
