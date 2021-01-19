d78e8350430660a17ecc9d6e648022679abdfb07
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2021-01-19 02:20:32 +0100

    The MidiIn Class does now handle reconnecting MIDI devices and automatically reconnects the callbacks to the devices

--- EarTrumpet/DataModel/MIDI/MidiIn.cs
+++ EarTrumpet/DataModel/MIDI/MidiIn.cs
@@ -9,6 +9,104 @@ namespace EarTrumpet.DataModel.MIDI
 {
     public static class MidiIn
     {
+        // This type is just awful
+        private static Dictionary<string, Dictionary<Tuple<byte, byte>, List<Action<MidiControlChangeMessage>>>>
+            callbacks;
+        private static DeviceWatcher deviceWatcher;
+        private static List<string> watchedDevices;
+        
+        internal static void AddControlChangeCallback(string id, Action<MidiControlChangeMessage> callback,
+            byte channel = 255, byte controller = 255)
+        {
+            if (!callbacks.ContainsKey(id))
+            {
+                callbacks.Add(id, new Dictionary<Tuple<byte, byte>, List<Action<MidiControlChangeMessage>>>());
+                _StartListening(id);
+            }
+            
+            var key = new Tuple<byte, byte>(channel, controller);
+            if (!callbacks[id].ContainsKey(key))
+            {
+                callbacks[id].Add(key, new List<Action<MidiControlChangeMessage>>());
+            }
+            
+            callbacks[id][key].Add(callback);
+        }
+
+        private static void MessageReceived(MidiInPort sender, MidiMessageReceivedEventArgs args)
+        {
+            IMidiMessage received = args.Message;
+
+            if (received.Type == MidiMessageType.ControlChange)
+            {
+                string id = sender.DeviceId;
+                if (!callbacks.ContainsKey(id))
+                {
+                    return;
+                }
+                
+                MidiControlChangeMessage msg = (MidiControlChangeMessage) received;
+                
+                var key = new Tuple<byte, byte>(msg.Channel, msg.Controller);
+                if (callbacks[id].ContainsKey(key))
+                {
+                    foreach (var callback in callbacks[id][key])
+                    {
+                        callback(msg);
+                    }
+                }
+
+                var universalKey = new Tuple<byte, byte>(255, 255);
+                if (callbacks[id].ContainsKey(universalKey))
+                {
+                    foreach (var callback in callbacks[id][universalKey])
+                    {
+                        callback(msg);
+                    }
+                }
+            }
+        }
+
+        private static void _StartListening(string id)
+        {
+            async Task StartListening()
+            {
+                var inPort = await MidiInPort.FromIdAsync(id);
+            
+                inPort.MessageReceived += MessageReceived;
+            }
+            Task.Run(async () => await StartListening()).Wait();
+            watchedDevices.Add(id);
+        }
+        
+        private static void Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            if (callbacks.ContainsKey(args.Id))
+            {
+                _StartListening(args.Id);
+            }
+        }
+        
+        private static void Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            if (watchedDevices.Contains(args.Id))
+            {
+                watchedDevices.Remove(args.Id);
+            }
+        }
+
+        static MidiIn()
+        {
+            callbacks = new Dictionary<string, Dictionary<Tuple<byte, byte>, List<Action<MidiControlChangeMessage>>>>();
+            watchedDevices = new List<string>();
+            
+            deviceWatcher = DeviceInformation.CreateWatcher(MidiInPort.GetDeviceSelector());
+            deviceWatcher.Added += Added;
+            deviceWatcher.Removed += Removed;
+            
+            deviceWatcher.Start();
+        }
+        
         private static async Task<List<MidiInDevice>> _GetAllDevices(bool returnEmptyNames=false)
         {
             var midiInputQueryString = MidiInPort.GetDeviceSelector();
--- EarTrumpet/DataModel/MIDI/MidiInDevice.cs
+++ EarTrumpet/DataModel/MIDI/MidiInDevice.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Midi;
 
@@ -9,8 +7,6 @@ namespace EarTrumpet.DataModel.MIDI
     public class MidiInDevice
     {
         private readonly DeviceInformation _device;
-        private MidiInPort _inPort;
-        private Dictionary<Tuple<byte, byte>, List<Action<MidiControlChangeMessage>>> _ccCallbacks;
 
         public string Name
         {
@@ -25,66 +21,12 @@ namespace EarTrumpet.DataModel.MIDI
         public MidiInDevice(DeviceInformation device)
         {
             _device = device;
-            _ccCallbacks = new Dictionary<Tuple<byte, byte>, List<Action<MidiControlChangeMessage>>>();
-        }
-        
-        public void AddControlChangeCallback(Action<MidiControlChangeMessage> callback, byte channel=255, byte controller=255)
-        {
-            if (_ccCallbacks.Count == 0)
-            {
-                _StartListening();
-            }
-
-            var key = new Tuple<byte, byte>(channel, controller);
-
-            if (!_ccCallbacks.ContainsKey(key))
-            {
-                _ccCallbacks.Add(key, new List<Action<MidiControlChangeMessage>>());
-            }
-            
-            _ccCallbacks[key].Add(callback);
         }
 
-        private async Task __StartListening()
+        public void AddControlChangeCallback(Action<MidiControlChangeMessage> callback, byte channel = 255,
+            byte controller = 255)
         {
-            _inPort = await MidiInPort.FromIdAsync(Id);
-
-            _inPort.MessageReceived += MessageReceived;
-        }
-
-        private void _StartListening()
-        {
-            var t = Task.Run(async () => await __StartListening());
-            t.Wait();
-        }
-
-        private void MessageReceived(MidiInPort sender, MidiMessageReceivedEventArgs args)
-        {
-            IMidiMessage received = args.Message;
-            
-            if (received.Type == MidiMessageType.ControlChange)
-            {
-                MidiControlChangeMessage msg = (MidiControlChangeMessage) received;
-
-                var key = new Tuple<byte, byte>(msg.Channel, msg.Controller);
-                if (_ccCallbacks.ContainsKey(key))
-                {
-                    foreach (var callback in _ccCallbacks[key])
-                    {
-                        callback(msg);
-                    }
-                }
-
-                var universalKey = new Tuple<byte, byte>(255, 255);
-                if (_ccCallbacks.ContainsKey(universalKey))
-                {
-                    foreach (var callback in _ccCallbacks[universalKey])
-                    {
-                        callback(msg);
-                    }
-                }
-            }
-            
+            MidiIn.AddControlChangeCallback(Id, callback, channel, controller);
         }
     }
 }
