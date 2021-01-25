fa78f1935c089e7a861b7a829ad6b97fd1757e71
Sven W <svenwml@yahoo.de>
2021-01-25 22:46:00 +0100

    add default device command german translation, modify english version

--- EarTrumpet/Properties/Resources.Designer.cs
+++ EarTrumpet/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace EarTrumpet.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -819,7 +819,7 @@ namespace EarTrumpet.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Set Default Device.
+        ///   Looks up a localized string similar to Set As Default Device.
         /// </summary>
         public static string SetAsDefaultDevice {
             get {
