f3694825e09a2bde97a53afd96fe8ea205a71eb0
GoldenTao <taoca@hotmail.com>
2021-02-26 15:51:10 +1000

    Fixed slow window drag on arcylic windows. Closes #349

--- EarTrumpet/UI/Themes/AcrylicBrush.cs
+++ EarTrumpet/UI/Themes/AcrylicBrush.cs
@@ -1,5 +1,9 @@
-﻿using EarTrumpet.Interop.Helpers;
+﻿using EarTrumpet.Extensions;
+using EarTrumpet.Interop.Helpers;
+using System;
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace EarTrumpet.UI.Themes
 {
@@ -9,11 +13,32 @@ namespace EarTrumpet.UI.Themes
         public static void SetBackground(DependencyObject obj, string value) => obj.SetValue(BackgroundProperty, value);
         public static readonly DependencyProperty BackgroundProperty =
         DependencyProperty.RegisterAttached("Background", typeof(string), typeof(AcrylicBrush), new PropertyMetadata("", BackgroundChanged));
+
+        public static bool GetIsSuppressed(DependencyObject obj) => (bool)obj.GetValue(IsSuppressedProperty);
+        public static void SetIsSuppressed(DependencyObject obj, bool value) => obj.SetValue(IsSuppressedProperty, value);
+        public static readonly DependencyProperty IsSuppressedProperty =
+        DependencyProperty.RegisterAttached("IsSuppressed", typeof(bool), typeof(AcrylicBrush), new PropertyMetadata(false));
+
         private static void BackgroundChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
             var window = (Window)dependencyObject;
+            var suppressAryclicTimer = new DispatcherTimer();
+
             window.Closed += (_, __) => window = null;
             window.SourceInitialized += (_, __) => ApplyAcrylicToWindow(window, (string)e.NewValue);
+            window.LocationChanged += (_, __) => SuppressAryclic(window, suppressAryclicTimer);
+            window.SizeChanged += (_, __) => SuppressAryclic(window, suppressAryclicTimer);
+
+            suppressAryclicTimer.Interval = TimeSpan.FromMilliseconds(200);
+            suppressAryclicTimer.Tick += (_, __) =>
+            {
+                suppressAryclicTimer.Stop();
+                if (window != null)
+                {
+                    SetIsSuppressed(window, false);
+                    UpdateWindowAcrylic(window);
+                }
+            };
 
             Manager.Current.ThemeChanged += () =>
             {
@@ -24,11 +49,50 @@ namespace EarTrumpet.UI.Themes
             };
         }
 
+        private static void SuppressAryclic(Window window, DispatcherTimer timer)
+        {
+            if (window != null && (HwndSource)PresentationSource.FromVisual(window) != null)
+            {
+                if (!timer.IsEnabled)
+                {
+                    SetIsSuppressed(window, true);
+
+                    // This works to prevent flicker:
+                    window.InvalidateVisual();
+                    window.UpdateLayout();
+                    Dispatcher.CurrentDispatcher.InvokeAsync(() => {
+                        if (window != null)
+                        {
+                            UpdateWindowAcrylic(window);
+                        }
+                    }, DispatcherPriority.Render);
+                }
+                timer.Stop();
+                timer.Start();
+            }
+        }
+
         private static void ApplyAcrylicToWindow(Window window, string refValue)
         {
-            AccentPolicyLibrary.EnableAcrylic(window,
-                Manager.Current.ResolveRef(window, refValue),
-                Interop.User32.AccentFlags.DrawAllBorders);
+            if (window != null)
+            {
+                AccentPolicyLibrary.EnableAcrylic(window,
+                    Manager.Current.ResolveRef(window, refValue),
+                    Interop.User32.AccentFlags.DrawAllBorders);
+            }
+        }
+
+        private static void UpdateWindowAcrylic(Window window)
+        {
+            var suppressed = GetIsSuppressed(window);
+            if (suppressed)
+            {
+                AccentPolicyLibrary.DisableAcrylic(window);
+            }
+            else
+            {
+                ApplyAcrylicToWindow(window, GetBackground(window));
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -43,7 +43,6 @@ Install EarTrumpet from the [Microsoft Store](https://www.microsoft.com/en-us/p/
 ## Known issues
 - SoundCloud audio cannot be adjusted ([#178](https://github.com/File-New-Project/EarTrumpet/issues/178))
 - Error: Windows cannot find "[...] EarTrumpet.exe". Make sure you typed the name correctly, and then try again ([#274](https://github.com/File-New-Project/EarTrumpet/issues/274))
-- EarTrumpet windows move slowly on Windows 10 19H1+ ([#349](https://github.com/File-New-Project/EarTrumpet/issues/349))
 
 ## Credits
 - David Golden ([@GoldenTao](https://www.twitter.com/GoldenTao))
