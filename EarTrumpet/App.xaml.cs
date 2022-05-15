fd6c67784f2c8c933443d403f96cacc0d16f5545
Pg <pg.developper.fr@gmail.com>
2022-05-15 19:31:04 +0200

    Add hotkeys to control volume of multiple devices at once (#1084)

--- EarTrumpet/App.xaml.cs
+++ EarTrumpet/App.xaml.cs
@@ -101,6 +101,8 @@ namespace EarTrumpet
             _settings.FlyoutHotkeyTyped += () => _flyoutViewModel.OpenFlyout(InputType.Keyboard);
             _settings.MixerHotkeyTyped += () => _mixerWindow.OpenOrClose();
             _settings.SettingsHotkeyTyped += () => _settingsWindow.OpenOrBringToFront();
+            _settings.AbsoluteVolumeUpHotkeyTyped += AbsoluteVolumeIncrement;
+            _settings.AbsoluteVolumeDownHotkeyTyped += AbsoluteVolumeDecrement;
             _settings.RegisterHotkeys();
 
             _trayIcon.PrimaryInvoke += (_, type) => _flyoutViewModel.OpenFlyout(type);
@@ -250,5 +252,32 @@ namespace EarTrumpet
         }
 
         private Window CreateMixerExperience() => new FullWindow { DataContext = new FullWindowViewModel(CollectionViewModel) };
+
+        private void AbsoluteVolumeIncrement()
+        {
+            foreach (var device in CollectionViewModel.AllDevices.Where(d => !d.IsMuted || d.IsAbsMuted))
+            {
+                // in any case this device is not abs muted anymore
+                device.IsAbsMuted = false;
+                device.IncrementVolume(2);
+            }
+        }
+
+        private void AbsoluteVolumeDecrement()
+        {
+            foreach (var device in CollectionViewModel.AllDevices.Where(d => !d.IsMuted))
+            {
+                // if device is not muted but will be muted by 
+                bool wasMuted = device.IsMuted;
+                // device.IncrementVolume(-2);
+                device.Volume -= 2;
+                // if device is muted by this absolute down
+                // .IsMuted is not already updated
+                if (!wasMuted == (device.Volume <= 0))
+                {
+                    device.IsAbsMuted = true;
+                }
+            }
+        }
     }
 }
--- EarTrumpet/Properties/Resources.Designer.cs
+++ EarTrumpet/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace EarTrumpet.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1314,7 +1314,25 @@ namespace EarTrumpet.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to General.
+        ///   Looks up a localized string similar to Absolute Volume Down.
+        /// </summary>
+        public static string SettingsAbsoluteVolumeDownText {
+            get {
+                return ResourceManager.GetString("SettingsAbsoluteVolumeDownText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Absolute Volume Up.
+        /// </summary>
+        public static string SettingsAbsoluteVolumeUpText {
+            get {
+                return ResourceManager.GetString("SettingsAbsoluteVolumeUpText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar à General.
         /// </summary>
         public static string SettingsCategoryTitle {
             get {
