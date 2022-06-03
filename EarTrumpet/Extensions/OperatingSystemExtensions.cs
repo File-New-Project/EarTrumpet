80f541b58afbcbbf3665f713d473106550f71097
krlvm <51774833+krlvm@users.noreply.github.com>
2022-06-03 19:09:16 +0300

    Disable acrylic mitigation on builds < 19H1 and > Windows 11

--- EarTrumpet/Extensions/OperatingSystemExtensions.cs
+++ EarTrumpet/Extensions/OperatingSystemExtensions.cs
@@ -7,6 +7,7 @@ namespace EarTrumpet.Extensions
         RS3 = 16299,
         RS4 = 17134,
         RS5_1809 = 17763,
+        Version19H1 = 18362,
         Version21H2 = 21390,
         Windows11 = 22000,
     }
