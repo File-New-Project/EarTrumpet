91ada37d577e2ba071936561f17730cb4a7861ae
Rafael Rivera <rafael@withinwindows.com>
2023-01-24 21:29:29 -0800

    Ensure cursor position is up-to-date before checking bounds

--- EarTrumpet/UI/Helpers/ShellNotifyIcon.cs
+++ EarTrumpet/UI/Helpers/ShellNotifyIcon.cs
@@ -164,11 +164,14 @@ namespace EarTrumpet.UI.Helpers
             {
                 ScheduleDelayedIconInvalidation();
             }
-            else if (msg.Msg == User32.WM_INPUT &&
-                    InputHelper.ProcessMouseInputMessage(msg.LParam, ref _cursorPosition, out int wheelDelta) &&
-                    IsCursorWithinNotifyIconBounds() && wheelDelta != 0)
+            else if (msg.Msg == User32.WM_INPUT)
             {
-                Scrolled?.Invoke(this, wheelDelta);
+                _cursorPosition = System.Windows.Forms.Cursor.Position;
+                if (InputHelper.ProcessMouseInputMessage(msg.LParam, ref _cursorPosition, out int wheelDelta) &&
+                                IsCursorWithinNotifyIconBounds() && wheelDelta != 0)
+                {
+                    Scrolled?.Invoke(this, wheelDelta);
+                }
             }
         }
 
