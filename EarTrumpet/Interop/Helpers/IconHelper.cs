8c67362bd77747878fe8fa7fdfaf3149238e5df2
Simplestas <tihiy.mozg@gmail.com>
2025-01-31 18:52:00 +0400

    Fix scenarios where icon is blurry
    - Using LoadIconWithScaleDown will ensure better-looking icon at non-standard DPI
    - Notification area can update app icon itself for proper DPI if GetIconInfoEx will return proper module path; this doesn't work for LOAD_LIBRARY_AS_IMAGE_RESOURCE or LOAD_LIBRARY_AS_DATAFILE mapping unfortunately so use DONT_RESOLVE_DLL_REFERENCES
    
    #495

--- EarTrumpet/Interop/Helpers/IconHelper.cs
+++ EarTrumpet/Interop/Helpers/IconHelper.cs
@@ -43,19 +43,12 @@ public class IconHelper
 
     public static Icon LoadIconResource(string path, int iconOrdinal, int cx, int cy)
     {
-        using var hModule = PInvoke.LoadLibraryEx(path, LOAD_LIBRARY_FLAGS.LOAD_LIBRARY_AS_DATAFILE | LOAD_LIBRARY_FLAGS.LOAD_LIBRARY_AS_IMAGE_RESOURCE);
+        using var hModule = PInvoke.LoadLibraryEx(path, LOAD_LIBRARY_FLAGS.DONT_RESOLVE_DLL_REFERENCES);
         unsafe
         {
             var rawModuleHandle = new HMODULE(hModule.DangerousGetHandle().ToPointer());
-            var groupResInfo = PInvoke.FindResource(rawModuleHandle, new PCWSTR((char*)iconOrdinal), PInvoke.RT_GROUP_ICON);
-            var groupResData = PInvoke.LockResource(PInvoke.LoadResource(hModule, groupResInfo));
-            var iconId = PInvoke.LookupIconIdFromDirectoryEx((byte*)groupResData, true, cx, cy, IMAGE_FLAGS.LR_DEFAULTCOLOR);
-
-            var iconResInfo = PInvoke.FindResource(rawModuleHandle, new PCWSTR((char*)iconId), PInvoke.RT_ICON);
-            var iconResData = PInvoke.LockResource(PInvoke.LoadResource(hModule, iconResInfo));
-            var iconResSize = PInvoke.SizeofResource(hModule, iconResInfo);
-            var iconHandle = PInvoke.CreateIconFromResourceEx((byte*)iconResData, iconResSize, true, 0x00030000, cx, cy, IMAGE_FLAGS.LR_DEFAULTCOLOR);
-
+            HICON iconHandle;
+            PInvoke.LoadIconWithScaleDown(rawModuleHandle, new PCWSTR((char*)iconOrdinal), cx, cy, &iconHandle);
             return Icon.FromHandle(iconHandle).AsDisposableIcon();
         }
     }
